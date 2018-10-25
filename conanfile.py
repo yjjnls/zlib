beac8620f84f470abcd559155cde87737a29c446
Mingyi Zhang <zhangmingyi@kedacom.com>
2018-10-25 16:07:57 +0800

    fix inconsistent use of tabs and spaces in indentation for python3

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,9 @@ try:
     import conanos.conan.hacks.cmake
 except:
     if os.environ.get('EMSCRIPTEN_VERSIONS'):
-        raise Exception('Please use pip install conanos to patch conan for emscripten binding !')
+        raise Exception(
+            'Please use pip install conanos to patch conan for emscripten binding !')
+
 
 class ZlibConan(ConanFile):
     name = "zlib"
@@ -26,7 +28,7 @@ class ZlibConan(ConanFile):
         try:
             return self.settings.compiler == 'emcc'
         except:
-		    return False
+            return False
 
     def configure(self):
         del self.settings.compiler.libcxx
@@ -38,7 +40,8 @@ class ZlibConan(ConanFile):
 
     def source(self):
         z_name = "zlib-%s.tar.gz" % self.version
-        tools.download("https://zlib.net/zlib-%s.tar.gz" % self.version, z_name)
+        tools.download("https://zlib.net/zlib-%s.tar.gz" %
+                       self.version, z_name)
         tools.unzip(z_name)
         os.unlink(z_name)
         files.rmdir("%s/contrib" % self.ZIP_FOLDER_NAME)
@@ -65,7 +68,6 @@ class ZlibConan(ConanFile):
                                       'install(TARGETS zlib zlibstatic',
                                       'install(TARGETS zlib ')
 
-                                     
             files.mkdir("_build")
             with tools.chdir("_build"):
                 if not tools.os_info.is_windows and not self.is_emscripten():
@@ -81,22 +83,30 @@ class ZlibConan(ConanFile):
                         tools.replace_in_file("../configure", old_str, new_str)
 
                     if self.settings.os == "Windows":  # Cross building to Linux
-                        tools.replace_in_file("../configure", 'LDSHAREDLIBC="${LDSHAREDLIBC--lc}"', 'LDSHAREDLIBC=""')
+                        tools.replace_in_file(
+                            "../configure", 'LDSHAREDLIBC="${LDSHAREDLIBC--lc}"', 'LDSHAREDLIBC=""')
                     # Zlib configure doesnt allow this parameters
 
                     if self.settings.os == "iOS":
-                        tools.replace_in_file("../gzguts.h", '#ifdef _LARGEFILE64_SOURCE', '#include <unistd.h>\n\n#ifdef _LARGEFILE64_SOURCE')
+                        tools.replace_in_file(
+                            "../gzguts.h", '#ifdef _LARGEFILE64_SOURCE', '#include <unistd.h>\n\n#ifdef _LARGEFILE64_SOURCE')
 
                     if self.settings.os == "Windows" and tools.os_info.is_linux:
                         # Let our profile to declare what is needed.
-                        tools.replace_in_file("../win32/Makefile.gcc", 'LDFLAGS = $(LOC)', '')
-                        tools.replace_in_file("../win32/Makefile.gcc", 'AS = $(CC)', '')
-                        tools.replace_in_file("../win32/Makefile.gcc", 'AR = $(PREFIX)ar', '')
-                        tools.replace_in_file("../win32/Makefile.gcc", 'CC = $(PREFIX)gcc', '')
-                        tools.replace_in_file("../win32/Makefile.gcc", 'RC = $(PREFIX)windres', '')
+                        tools.replace_in_file(
+                            "../win32/Makefile.gcc", 'LDFLAGS = $(LOC)', '')
+                        tools.replace_in_file(
+                            "../win32/Makefile.gcc", 'AS = $(CC)', '')
+                        tools.replace_in_file(
+                            "../win32/Makefile.gcc", 'AR = $(PREFIX)ar', '')
+                        tools.replace_in_file(
+                            "../win32/Makefile.gcc", 'CC = $(PREFIX)gcc', '')
+                        tools.replace_in_file(
+                            "../win32/Makefile.gcc", 'RC = $(PREFIX)windres', '')
                         self.run("cd .. && make -f win32/Makefile.gcc")
                     else:
-                        env_build.configure("../", build=False, host=False, target=False)
+                        env_build.configure(
+                            "../", build=False, host=False, target=False)
                         env_build.make()
                 else:
                     cmake = CMake(self)
@@ -118,7 +128,8 @@ class ZlibConan(ConanFile):
         # Copy pc file
         self.copy("*.pc", dst="", keep_path=False)
         # Copying zlib.h, zutil.h, zconf.h
-        self.copy("*.h", "include", "%s" % self.ZIP_FOLDER_NAME, keep_path=False)
+        self.copy("*.h", "include", "%s" %
+                  self.ZIP_FOLDER_NAME, keep_path=False)
         self.copy("*.h", "include", "%s" % "_build", keep_path=False)
 
         # Copying static and dynamic libs
@@ -126,19 +137,25 @@ class ZlibConan(ConanFile):
         lib_path = os.path.join(self.package_folder, "lib")
         suffix = "d" if self.settings.build_type == "Debug" else ""
         if self.is_emscripten():
-            self.copy(pattern="*.so*", dst="lib", src=build_dir, keep_path=False)
+            self.copy(pattern="*.so*", dst="lib",
+                      src=build_dir, keep_path=False)
             return
 
         if self.settings.os == "Windows":
             if self.options.shared:
                 build_dir = os.path.join(self.ZIP_FOLDER_NAME, "_build")
-                self.copy(pattern="*.dll", dst="bin", src=build_dir, keep_path=False)
+                self.copy(pattern="*.dll", dst="bin",
+                          src=build_dir, keep_path=False)
                 build_dir = os.path.join(self.ZIP_FOLDER_NAME, "_build/lib")
-                self.copy(pattern="*zlibd.lib", dst="lib", src=build_dir, keep_path=False)
-                self.copy(pattern="*zlib.lib", dst="lib", src=build_dir, keep_path=False)
-                self.copy(pattern="*zlib.dll.a", dst="lib", src=build_dir, keep_path=False)
+                self.copy(pattern="*zlibd.lib", dst="lib",
+                          src=build_dir, keep_path=False)
+                self.copy(pattern="*zlib.lib", dst="lib",
+                          src=build_dir, keep_path=False)
+                self.copy(pattern="*zlib.dll.a", dst="lib",
+                          src=build_dir, keep_path=False)
                 if tools.os_info.is_linux:
-                    self.copy(pattern="*libz.dll.a", dst="lib", src=self.ZIP_FOLDER_NAME)
+                    self.copy(pattern="*libz.dll.a", dst="lib",
+                              src=self.ZIP_FOLDER_NAME)
                 if self.settings.compiler == "Visual Studio":
                     current_lib = os.path.join(lib_path, "zlib%s.lib" % suffix)
                     os.rename(current_lib, os.path.join(lib_path, "zlib.lib"))
@@ -147,28 +164,38 @@ class ZlibConan(ConanFile):
                 if self.settings.os == "Windows":
                     if tools.os_info.is_windows:
                         # MinGW
-                        self.copy(pattern="libzlibstaticd.a", dst="lib", src=build_dir, keep_path=False)
-                        self.copy(pattern="libzlibstatic.a", dst="lib", src=build_dir, keep_path=False)
+                        self.copy(pattern="libzlibstaticd.a",
+                                  dst="lib", src=build_dir, keep_path=False)
+                        self.copy(pattern="libzlibstatic.a", dst="lib",
+                                  src=build_dir, keep_path=False)
                         # Visual Studio
-                        self.copy(pattern="zlibstaticd.lib", dst="lib", src=build_dir, keep_path=False)
-                        self.copy(pattern="zlibstatic.lib", dst="lib", src=build_dir, keep_path=False)
+                        self.copy(pattern="zlibstaticd.lib", dst="lib",
+                                  src=build_dir, keep_path=False)
+                        self.copy(pattern="zlibstatic.lib", dst="lib",
+                                  src=build_dir, keep_path=False)
                     if tools.os_info.is_linux:
-                        self.copy(pattern="libz.a", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
+                        self.copy(pattern="libz.a", dst="lib",
+                                  src=self.ZIP_FOLDER_NAME, keep_path=False)
                 if self.settings.compiler == "Visual Studio":
-                    current_lib = os.path.join(lib_path, "zlibstatic%s.lib" % suffix)
+                    current_lib = os.path.join(
+                        lib_path, "zlibstatic%s.lib" % suffix)
                     os.rename(current_lib, os.path.join(lib_path, "zlib.lib"))
                 elif self.settings.compiler == "gcc":
                     if not tools.os_info.is_linux:
                         current_lib = os.path.join(lib_path, "libzlibstatic.a")
-                        os.rename(current_lib, os.path.join(lib_path, "libzlib.a"))
+                        os.rename(current_lib, os.path.join(
+                            lib_path, "libzlib.a"))
         else:
             if self.options.shared:
                 if self.settings.os == "Macos":
-                    self.copy(pattern="*.dylib", dst="lib", src=build_dir, keep_path=False)
+                    self.copy(pattern="*.dylib", dst="lib",
+                              src=build_dir, keep_path=False)
                 else:
-                    self.copy(pattern="*.so*", dst="lib", src=build_dir, keep_path=False)
+                    self.copy(pattern="*.so*", dst="lib",
+                              src=build_dir, keep_path=False)
             else:
-                self.copy(pattern="*.a", dst="lib", src=build_dir, keep_path=False)
+                self.copy(pattern="*.a", dst="lib",
+                          src=build_dir, keep_path=False)
 
     def package_info(self):
         if self.is_emscripten():
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,6 +1,7 @@
 from conans.model.conan_file import ConanFile, tools
 from conans import CMake
-import os, platform
+import os
+import platform
 
 
 class DefaultNameConan(ConanFile):
@@ -13,7 +14,7 @@ class DefaultNameConan(ConanFile):
         try:
             return self.settings.compiler == 'emcc'
         except:
-		    return False
+            return False
 
     def configure(self):
         del self.settings.compiler.libcxx
@@ -24,21 +25,21 @@ class DefaultNameConan(ConanFile):
     def build(self):
 
         # Compatibility with Conan < 1.0 (#2234)
-        generator = "Unix Makefiles" if platform.system() != "Windows" else None 
+        generator = "Unix Makefiles" if platform.system() != "Windows" else None
         cmake = CMake(self, generator=generator)
         cmake.configure()
         cmake.build()
 
-    def imports(self):        
+    def imports(self):
         self.copy(pattern="*.dll", dst="bin", src="bin")
         self.copy(pattern="*.dylib", dst="bin", src="lib")
-        
+
     def test(self):
         if self.is_emscripten():
-            self.run( 'node bin/test.js')
+            self.run('node bin/test.js')
             return
-            
+
         if not tools.cross_building(self.settings):
             self.run("cd bin && .%stest" % os.sep)
-        assert os.path.exists(os.path.join(self.deps_cpp_info["zlib"].rootpath, "LICENSE"))
-
+        assert os.path.exists(os.path.join(
+            self.deps_cpp_info["zlib"].rootpath, "LICENSE"))
