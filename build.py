b7b4800d002f0524f93df881985d2a547852b6e3
yjjnls <x-jj@foxmail.com>
2018-11-26 16:12:17 +0800

    .

--- build.py
+++ build.py
@@ -1,5 +1,34 @@
 #!/usr/bin/env python
 from conanos.build import Main
+from conan.packager import ConanMultiPackager
+import os
 
 if __name__ == "__main__":    
-    Main('zlib',pure_c=True)
+    # Main('zlib',pure_c=True)
+    builder = ConanMultiPackager()
+    builder.add_common_builds(shared_option_name="zlib:shared", pure_c=True)
+
+    if os.environ.get('EMSCRIPTEN_VERSIONS'):
+        for version in os.environ['EMSCRIPTEN_VERSIONS'].split(','):
+            for build_type in os.environ.get('CONAN_BUILD_TYPES','Debug').split(','):
+                builder.add(settings={
+                    "arch": "any", 
+                    "compiler": "emcc",
+                    "compiler.libcxx":'libstdc++11',
+                    "build_type": build_type, 
+                    "compiler.version": version
+                    },
+                    options={'zlib:shared':True}, env_vars={}, build_requires={})
+
+        items = []
+        for item in builder.items:
+            if not os.environ.get('CONAN_GCC_VERSIONS') and item.settings['compiler'] == 'gcc':
+                continue  
+            if not os.environ.get('CONAN_CLANG_VERSIONS') and item.settings['compiler'] == 'clang':
+                continue 
+            items.append(item)
+
+        builder.items = items
+
+
+    builder.run()
